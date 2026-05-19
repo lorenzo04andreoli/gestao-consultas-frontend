426f9dc23eaa1c63d3ac6e33f302555ba9dd5526
lorenzo04andreoli <lorenzo04andreoli@gmail.com>
2026-05-19 16:54:24 -0300

    feat(relatorios): adiciona filtros de consultas

--- src/app/features/relatorios/relatorios-page/relatorios-page.ts
+++ src/app/features/relatorios/relatorios-page/relatorios-page.ts
@@ -1,8 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ConsultaFiltros } from '../../consultas/consulta.model';
+import { DentistaService } from '../../dentistas/dentista';
+import { DentistaResponseModel } from '../../dentistas/dentista.model';
+import { EspecialidadeService } from '../../especialidades/especialidade';
+import { EspecialidadeModel } from '../../especialidades/especialidade.model';
+import { PacienteService } from '../../pacientes/paciente';
+import { PacienteModel } from '../../pacientes/paciente.model';
 
 @Component({
   selector: 'app-relatorios-page',
   standalone: true,
-  template: ''
+  imports: [FormsModule],
+  templateUrl: './relatorios-page.html'
 })
-export class RelatoriosPage {}
+export class RelatoriosPage implements OnInit {
+  private pacienteService = inject(PacienteService);
+  private dentistaService = inject(DentistaService);
+  private especialidadeService = inject(EspecialidadeService);
+
+  pacientes = signal<PacienteModel[]>([]);
+  dentistas = signal<DentistaResponseModel[]>([]);
+  especialidades = signal<EspecialidadeModel[]>([]);
+  erro = signal('');
+
+  filtros: ConsultaFiltros = {
+    pacienteId: null,
+    dentistaId: null,
+    especialidadeId: null,
+    dataInicio: '',
+    dataFim: ''
+  };
+
+  ngOnInit() {
+    this.carregarFiltros();
+  }
+
+  carregarFiltros() {
+    this.pacienteService.listar().subscribe({
+      next: (dados) => this.pacientes.set(dados),
+      error: () => this.erro.set('Erro ao carregar pacientes.')
+    });
+
+    this.dentistaService.listar().subscribe({
+      next: (dados) => this.dentistas.set(dados),
+      error: () => this.erro.set('Erro ao carregar dentistas.')
+    });
+
+    this.especialidadeService.listar().subscribe({
+      next: (dados) => this.especialidades.set(dados),
+      error: () => this.erro.set('Erro ao carregar especialidades.')
+    });
+  }
+
+  limparFiltros() {
+    this.filtros = {
+      pacienteId: null,
+      dentistaId: null,
+      especialidadeId: null,
+      dataInicio: '',
+      dataFim: ''
+    };
+  }
+}
