091c3b15e4efb5199662c6ea69990e35cee84622
lorenzo04andreoli <lorenzo04andreoli@gmail.com>
2026-05-25 17:39:27 -0300

    feat(consultas): adiciona filtros na listagem

--- src/app/features/consultas/consultas-listar-page/consultas-listar-page.ts
+++ src/app/features/consultas/consultas-listar-page/consultas-listar-page.ts
@@ -1,7 +1,7 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { ConsultaModel } from '../consulta.model';
+import { ConsultaModel, StatusConsulta } from '../consulta.model';
 import { ConsultaService } from '../consulta';
 
 @Component({
@@ -17,6 +17,9 @@ export class ConsultasListarPage implements OnInit {
   consultas = signal<ConsultaModel[]>([]);
   erro = signal('');
   termoBusca = '';
+  statusFiltro: StatusConsulta | '' = '';
+  dataInicioFiltro = '';
+  dataFimFiltro = '';
 
   ngOnInit() {
     this.carregarConsultas();
@@ -43,16 +46,17 @@ export class ConsultasListarPage implements OnInit {
   consultasFiltradas() {
     const termo = this.normalizarTexto(this.termoBusca);
 
-    if (!termo) return this.consultas();
+    return this.consultas()
+      .filter(consulta => !termo || this.consultaContemTermo(consulta, termo))
+      .filter(consulta => !this.statusFiltro || consulta.status === this.statusFiltro)
+      .filter(consulta => this.consultaDentroDoPeriodo(consulta));
+  }
 
-    return this.consultas().filter(consulta =>
-      [
-        consulta.pacienteNome,
-        consulta.dentistaNome,
-        consulta.descricao,
-        consulta.status
-      ].some(valor => this.normalizarTexto(valor).includes(termo))
-    );
+  limparFiltros() {
+    this.termoBusca = '';
+    this.statusFiltro = '';
+    this.dataInicioFiltro = '';
+    this.dataFimFiltro = '';
   }
 
   private extrairMensagemErro(err: unknown) {
@@ -68,5 +72,29 @@ export class ConsultasListarPage implements OnInit {
       .toLowerCase()
       .normalize('NFD')
       .replace(/[\u0300-\u036f]/g, '');
+  }
+
+  private consultaContemTermo(consulta: ConsultaModel, termo: string) {
+    return [
+      consulta.pacienteNome,
+      consulta.dentistaNome,
+      consulta.descricao,
+      consulta.status
+    ].some(valor => this.normalizarTexto(valor).includes(termo));
+  }
+
+  private consultaDentroDoPeriodo(consulta: ConsultaModel) {
+    const inicioConsulta = new Date(consulta.dataInicio).getTime();
+    const inicioFiltro = this.dataInicioFiltro
+      ? new Date(`${this.dataInicioFiltro}T00:00:00`).getTime()
+      : null;
+    const fimFiltro = this.dataFimFiltro
+      ? new Date(`${this.dataFimFiltro}T23:59:59`).getTime()
+      : null;
+
+    if (inicioFiltro && inicioConsulta < inicioFiltro) return false;
+    if (fimFiltro && inicioConsulta > fimFiltro) return false;
+
+    return true;
   }
 }
