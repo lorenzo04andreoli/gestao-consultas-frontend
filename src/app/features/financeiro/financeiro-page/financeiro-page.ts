c9f85a2328265dc64dd28cd36fa676bfcea897a9
lorenzo04andreoli <lorenzo04andreoli@gmail.com>
2026-06-04 21:04:27 -0300

    feat(financeiro): cria pagina de resumo

--- src/app/features/financeiro/financeiro-page/financeiro-page.ts
+++ src/app/features/financeiro/financeiro-page/financeiro-page.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { FinanceiroService } from '../financeiro';
+import { FinanceiroResumoModel } from '../financeiro.model';
 
 @Component({
   selector: 'app-financeiro-page',
@@ -6,4 +9,45 @@ import { Component } from '@angular/core';
   templateUrl: './financeiro-page.html',
   styleUrl: './financeiro-page.scss'
 })
-export class FinanceiroPage {}
+export class FinanceiroPage implements OnInit {
+  resumo = signal<FinanceiroResumoModel | null>(null);
+  carregando = signal(true);
+  erro = signal('');
+
+  constructor(private financeiroService: FinanceiroService) {}
+
+  ngOnInit() {
+    this.carregarResumo();
+  }
+
+  carregarResumo() {
+    this.carregando.set(true);
+    this.erro.set('');
+
+    this.financeiroService.resumo().subscribe({
+      next: resumo => {
+        this.resumo.set(resumo);
+        this.carregando.set(false);
+      },
+      error: err => {
+        this.erro.set(this.extrairMensagemErro(err, 'Erro ao carregar resumo financeiro.'));
+        this.carregando.set(false);
+      }
+    });
+  }
+
+  formatarMoeda(valor?: number | null) {
+    return new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL'
+    }).format(valor ?? 0);
+  }
+
+  private extrairMensagemErro(err: unknown, mensagemPadrao: string) {
+    if (err instanceof HttpErrorResponse) {
+      return err.error?.message ?? err.error?.erro ?? mensagemPadrao;
+    }
+
+    return mensagemPadrao;
+  }
+}
