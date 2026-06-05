2c9ba7611c68997dcae58293be91286ac9c2411f
lorenzo04andreoli <lorenzo04andreoli@gmail.com>
2026-06-04 21:00:11 -0300

    feat(financeiro): adiciona rota e menu

---
+++ src/app/features/financeiro/financeiro-page/financeiro-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-financeiro-page',
+  standalone: true,
+  templateUrl: './financeiro-page.html',
+  styleUrl: './financeiro-page.scss'
+})
+export class FinanceiroPage {}
